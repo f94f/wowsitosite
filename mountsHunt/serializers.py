786d92c8e7dcfa964e59f407ec2834d14c1959c1
f94f <f.mate10@uniandes.edu.co>
2021-04-01 01:20:42 +0200

    remove serialaizer

--- mountsHunt/serializers.py
+++
@@ -1,8 +0,0 @@
-from mountsHunt.models import Mount
-
-class MountSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Mount
-        fields = ['name', 'expansion', 'notes_1', 'notes_2', 'requirements', 'source', 'url_img']
-
-
