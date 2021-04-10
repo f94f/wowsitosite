d7cc9dd8c51cb8f05e4313ff9bde6ad6b0c08f81
f94f <f.mate10@uniandes.edu.co>
2021-04-10 03:06:01 +0200

    update DB and view

--- mountsHunt/urls.py
+++ mountsHunt/urls.py
@@ -3,8 +3,10 @@ from mountsHunt import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('firstLoad', views.firstLoad, name='firstLoad'),
     path('mounts', views.allMounts, name='mounts'),
     path('mount/<str:pk>', views.getMount, name='mount'),
-    path('img/<int:pk>/<str:tipo>', views.img, name='img'),
+    path('filterMounts', views.filterMounts, name='filterMounts'),
     # path('load', views.load_mounts_form_json, name='load'),
+    # path('img/<int:pk>/<str:tipo>', views.img, name='img'),
 ]
--- mountsHunt/views.py
+++ mountsHunt/views.py
@@ -1,12 +1,16 @@
 import os
 import json
+import sys
 
+from django.db import connection
 from django.conf import settings
 from django.shortcuts import render
 from django.core.serializers import serialize
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
+
+from types import SimpleNamespace
 
 from mountsHunt.models import Mount
 
@@ -24,54 +28,172 @@ def index_old(request):
 def index(request):
     return render(request, 'mountsHunt/index.html')
 
-# @csrf_exempt
+@require_http_methods(["GET"])
+def firstLoad(request):
+    mounts = []
+    expansions = []
+    types = []
+    factions = []
+    zones = []
+    try:
+        cursor = connection.cursor()
+        cursor.execute("SELECT M.ID, M.Name, M.Image_mini, M.Image, M.URL_img, M.URL_img_min FROM MOUNT M ORDER BY M.Name")
+        columns = [column[0] for column in cursor.description]
+        for row in cursor.fetchall():
+            mounts.append(dict(zip(columns, row)))
+        
+        cursor.execute("SELECT E.ID, E.Name FROM EXPANSION E ORDER BY E.ID")
+        columns = [column[0] for column in cursor.description]
+        for row in cursor.fetchall():
+            expansions.append(dict(zip(columns, row)))
+        
+        cursor.execute("SELECT TM.ID, TM.Name FROM TYPE_MOUNT TM ORDER BY TM.ID")
+        columns = [column[0] for column in cursor.description]
+        for row in cursor.fetchall():
+            types.append(dict(zip(columns, row)))
+        
+        cursor.execute("SELECT F.ID, F.Name FROM FACTION F ORDER BY F.ID")
+        columns = [column[0] for column in cursor.description]
+        for row in cursor.fetchall():
+            factions.append(dict(zip(columns, row)))
+        
+        cursor.execute("SELECT Z.ID, Z.Name FROM ZONE Z ORDER BY Z.ID")
+        columns = [column[0] for column in cursor.description]
+        for row in cursor.fetchall():
+            zones.append(dict(zip(columns, row)))
+    except:
+        mounts = []
+    
+    data = { 'mounts': mounts, 'expansions': expansions, 'types': types, 'factions': factions, 'zones': zones }
+    return HttpResponse(json.dumps(data), content_type="application/json")
+
 @require_http_methods(["GET"])
 def allMounts(request):
-    try:
-        mounts = Mount.objects.all()
-    except Mount.DoesNotExist:
-        mounts = []
     data = []
-    for m in mounts:
-        mount = { 
-            "id": m.id,
-            "name": m.name,
-            # "expansion": m.expansion,
-            # "notes_1": m.notes_1,
-            # "notes_2": m.notes_2,
-            # "requirements": m.requirements,
-            # "source": m.source,
-            # "url_info": m.url_info,
-            # "url_wowhead": m.url_wowhead,
-            "image_mini": m.image_mini,
-            # "image": m.image,
-            # "url_img": m.url_img,
-            "url_img_min": m.url_img_min
-        }
-        data.append(mount)
+    try:
+        cursor = connection.cursor()
+        cursor.execute("SELECT M.ID, M.Name, M.Image_mini, M.Image, M.URL_img, M.URL_img_min FROM MOUNT M ORDER BY M.Name")
+        columns = [column[0] for column in cursor.description]
+        for row in cursor.fetchall():
+            data.append(dict(zip(columns, row)))
+    except:
+        data = []
     return HttpResponse(json.dumps(data), content_type="application/json")
 
 @require_http_methods(["GET"])
 def getMount(request, pk):
+    data = None
     try:
-        m = Mount.objects.filter(pk=pk).first()
-    except Mount.DoesNotExist:
-        m = None
-    data = {
-        "id": m.id,
-        "name": m.name,
-        "expansion": m.expansion,
-        "notes_1": m.notes_1,
-        "notes_2": m.notes_2,
-        "requirements": m.requirements,
-        "source": m.source,
-        "url_info": m.url_info,
-        "url_wowhead": m.url_wowhead,
-        "image_mini": m.image_mini,
-        "image": m.image,
-        "url_img": m.url_img,
-        "url_img_min": m.url_img_min
-    }
+        print(pk)
+        cursor = connection.cursor()
+        cursor.execute("SELECT M.ID , M.Name, E.Name Expansion, M.Notes_1, M.Notes_2, M.Requirements, M.Source, "
+                    "Z.Name Zone, M.URL_info, M.URL_wowhead, M.Image_mini, M.Image, M.URL_img, M.URL_img_min, "
+                    "F.Name Faction, TM.Name Type, TT.Name Type_Timing "
+                    "FROM MOUNT M "
+                    "LEFT OUTER JOIN EXPANSION E ON M.Expansion_id = E.ID "
+                    "LEFT OUTER JOIN FACTION F ON M.Faction_id = F.ID "
+                    "LEFT OUTER JOIN TYPE_MOUNT TM ON M.Type_id = TM.ID "
+                    "LEFT OUTER JOIN TYPE_TIMING TT ON M.Type_timing_id = TT.ID "
+                    "LEFT OUTER JOIN ZONE Z ON M.Zone_id = Z.ID "
+                    "WHERE M.ID = %s", [pk])
+        columns = [column[0] for column in cursor.description]
+        data = dict(zip(columns, cursor.fetchone()))
+    except:
+        print(sys.exc_info())
+        data = None
+    return HttpResponse(json.dumps(data), content_type="application/json")
+
+@csrf_exempt
+@require_http_methods(["POST"])
+def filterMounts(request):
+    print(request.body)
+    filter = json.loads(str(request.body, encoding='utf-8'))["filter"]
+    print(filter)
+    data = []
+    try:
+        query = "SELECT M.ID, M.Name, M.Image_mini, M.Image, M.URL_img, M.URL_img_min \
+        FROM MOUNT M \
+        LEFT OUTER JOIN EXPANSION E ON M.Expansion_id = E.ID \
+        LEFT OUTER JOIN FACTION F ON M.Faction_id = F.ID \
+        LEFT OUTER JOIN TYPE_MOUNT TM ON M.Type_id = TM.ID \
+        LEFT OUTER JOIN TYPE_TIMING TT ON M.Type_timing_id = TT.ID \
+        LEFT OUTER JOIN ZONE Z ON M.Zone_id = Z.ID "
+
+        params = []
+        # NAME
+        if not filter["name"]:
+            query = query + "WHERE M.Name LIKE %s "
+            params.append("%")
+        else:
+            query = query + "WHERE M.Name LIKE %s "
+            params.append("%" + filter["name"] + "%")
+        
+        # EXPANSIONE
+        if len(filter["expansions"]) == 0 or "0" in filter["expansions"]:
+            query = query + "AND (E.ID LIKE %s OR E.ID IS NULL) "
+            params.append("%")
+        else:
+            count = 0
+            query = query + "AND (E.ID IN ("
+            for e in filter["expansions"]:
+                count = count + 1
+                query = query + " %s "
+                if len(filter["expansions"]) > 1 and count < len(filter["expansions"]):
+                    query = query + ","
+                params.append(e)
+            query = query + ") OR E.ID IS NULL) "
+
+        # TYPE
+        if len(filter["type"]) == 0 or "all" in filter["type"]:
+            query = query + "AND (TM.ID LIKE %s OR TM.ID IS NULL) "
+            params.append("%")
+        else:
+            count = 0
+            query = query + "AND TM.ID IN ("
+            for e in filter["type"]:
+                count = count + 1
+                query = query + " %s "
+                if len(filter["type"]) > 1 and count < len(filter["type"]):
+                    query = query + ","
+                params.append(e)
+            query = query + ") "
+
+        # FACTION
+        if filter["faction"] == 1:
+            query = query + "AND (F.ID LIKE %s OR F.ID IS NULL) "
+            params.append("%")
+        else:
+            query = query + "AND F.ID = %s "
+            params.append(filter["faction"])
+        
+        # ZONE
+        if len(filter["zone"]) == 0 or "all" in filter["zone"]:
+            query = query + "AND (Z.ID LIKE %s OR Z.ID IS NULL) "
+            params.append("%")
+        else:
+            count = 0
+            query = query + "AND Z.ID IN ("
+            for e in filter["zone"]:
+                count = count + 1
+                query = query + " %s "
+                if len(filter["zone"]) > 1 and count < len(filter["zone"]):
+                    query = query + ","
+                params.append(e)
+            query = query + ") "
+
+        query = query + "ORDER BY M.Name"
+        print(query)
+        print(params)
+
+        cursor = connection.cursor()
+        cursor.execute(query, params)
+        columns = [column[0] for column in cursor.description]
+        for row in cursor.fetchall():
+            data.append(dict(zip(columns, row)))
+    except:
+        print(sys.exc_info())
+        data = None
+    
     return HttpResponse(json.dumps(data), content_type="application/json")
 
 #################################
@@ -97,10 +219,37 @@ def img(request, pk, tipo):
 def load_mounts_form_json(request):
     path = os.getcwd() + '/mountsHunt/data/'
     file_list = os.listdir(path)
+
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM MOUNT")
     for f in file_list:
         with open(path + f, encoding='utf-8') as data_file:
             json_data = json.loads(data_file.read())
 
             for mount_data in json_data:
-                mount = Mount.create(**mount_data)
+                URL_img = mount_data["Id"] + "-" + mount_data["Name"].replace('/', ' ') + "/" + mount_data["Image"]
+                URL_img_min = mount_data["Id"] + "-" + mount_data["Name"].replace('/', ' ') + "/" + mount_data["ImageMini"]
+                params = [mount_data["Id"], mount_data["Name"], get_dict_expansion(mount_data["Expansion"]), mount_data["Notes_1"], mount_data["Notes_2"],
+                     mount_data["Requirements"], mount_data["Source"], None, mount_data["UrlInfo"], mount_data["UrlWowhead"], mount_data["ImageMini"],
+                     mount_data["Image"], URL_img, URL_img_min, None, None, None]
+                cursor.execute("INSERT INTO MOUNT (ID,Name,Expansion_id,Notes_1,Notes_2,Requirements,Source,Zone_id,"
+                    "URL_info,URL_wowhead,Image_mini,Image,URL_img,URL_img_min,Faction_id,Type_id,Type_timing_id) "
+                    "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", params)
     return HttpResponse("All good.")
+
+def get_dict_expansion(expansion):
+    try:
+        thisdict =	{
+            "Original": "1",
+            "The Burning Crusade": "2",
+            "Wrath of The Lich King": "3",
+            "Cataclysm": "4",
+            "Mist of Pandaria": "5",
+            "Warlords of Draenor": "6",
+            "Legion": "7",
+            "Battle for Azeroth": "8",
+            "Shadowlands": "9"
+        }
+        return thisdict[expansion]
+    except:
+        return "0"
